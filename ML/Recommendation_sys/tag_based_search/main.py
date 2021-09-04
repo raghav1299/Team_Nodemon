39d1e7fc22f7bbae6a5cd3194a5721d68ab2cba7
sd2001 <im.swarnabha2001@gmail.com>
2021-09-04 19:27:06 +0530

    🎨 add: added proper imports from directories

--- ML/Recommendation_sys/tag_based_search/main.py
+++ ML/Recommendation_sys/tag_based_search/main.py
@@ -3,8 +3,8 @@ Call connectdataabse from connect-db
 pass param to get data(cnx)
 
 '''
-import connect_db as db
-from data_processing import *
+from .connect_db import *
+from .data_processing import *
 
 # Pipeline :
 # cnx = db.connect_database()
@@ -14,7 +14,7 @@ from data_processing import *
 # final_pred = predict_recommendations(df_rec, product)
 
 def get_products(tag_input):
-    cnx = db.connect_database()
+    cnx = connect_database()
     try:
         df_tags = get_data_db(cnx, tag_input)
         df_json = data_to_dict(df_tags)
@@ -26,23 +26,6 @@ def get_products(tag_input):
         return final_dict
     except Exception as e:
         return {'status': 500, 'data' : []}
-
-# def predict_products(products):
-#     df = get_data_csv()
-#     df_rec= table_processing(df)
-#     vectorizer, X1 = tfidf_processing(df_rec)
-#     model, order_centroids, terms = kmeans_model(X1, vectorizer)
-#     vectorizer, X1 = tfidf_processing(df_rec)
-#     final_pred = predict_recommendations(df_rec, products, model, vectorizer, order_centroids, terms)
-#     return final_pred
-
-# def train_model():
-#     cnx = db.connect_database()
-#     df = get_data_db(cnx)
-#     df_rec= table_processing(df)
-#     vectorizer, X1 = tfidf_processing(df_rec)
-#     model, order_centroids, terms = kmeans_model(X1, vectorizer)
-    
 
 
 
