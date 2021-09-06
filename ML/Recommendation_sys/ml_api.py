c42b41ebe9efb2579bbd898abf21f4db463df13c
sd2001 <im.swarnabha2001@gmail.com>
2021-09-06 19:55:21 +0530

    ✨feat: added apriori based "Users also bought" recommendations

--- ML/Recommendation_sys/ml_api.py
+++ ML/Recommendation_sys/ml_api.py
@@ -3,6 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse, JSONResponse
 from tag_based_search import main
 from tag_based import main as main2
+from apriori import main as main3
 from pydantic import BaseModel
 
 
@@ -14,6 +15,7 @@ class Product_List(BaseModel):
 @app.get('/tags/{tag_input}')
 async def get_tag_products(tag_input: str):
     '''
+    Tag based search:
     Pass a tag in the url and get back the products only with that specific tag 
     '''
     response = main.get_products(tag_input)
@@ -22,11 +24,23 @@ async def get_tag_products(tag_input: str):
 @app.post('/recommend')
 async def get_product_recommendations(products: Product_List):
     '''
+    Recommended Items:
     post an array of product names and get back the most similar recommended products
     '''
     product_list = products.product_names
     final_products = main2.predict_products(product_list)
     return final_products
 
+@app.post('/alsobought')
+async def users_also_bought(products: Product_List):
+    '''
+    Users also Bought:
+    To be shown in the final cart during checkout:
+    post an array of product names and get back the most similar recommended products
+    '''
+    product_list = products.product_names
+    final_products = main3.other_user_orders_apriori(product_list)
+    return final_products
+    
 
     
