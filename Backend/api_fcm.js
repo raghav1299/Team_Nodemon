481d24d5bc7a2d8ccb1833dc3b11e700d9ec1219
Chetan Pareek <pareekchetan7891@gmail.com>
2021-09-09 00:33:37 +0530

    add: get_all_riders and get_details_using_username

---
+++ Backend/api_fcm.js
@@ -0,0 +1,47 @@
+var axios = require("axios").default;
+
+var options = {
+  method: "GET",
+  url: "https://distance-calculator1.p.rapidapi.com/v1/getdistance",
+  params: {
+    start_lat: "13.198989944266078",
+    start_lng: "77.70908188996312",
+    end_lat: "13.113908410795627",
+    end_lng: "77.5745906650984",
+    unit: "kilometers",
+  },
+  headers: {
+    "x-rapidapi-host": "distance-calculator1.p.rapidapi.com",
+    "x-rapidapi-key": "1446952fcdmsh4d730342fde0d6dp1f1453jsn49f9f83affe6",
+  },
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
+
+//send_notification_to_shop
+var data = JSON.stringify({
+  to: "dF2UQ3AsRcGapMtVy3BiiL:APA91bF7EyV7LER_h1RRhlWuBNze2kuHhhMvrJuEqWvVuPO5uccKqLIditGyZ1EmSaOcTSg9vhgtFGLeXhQYtv0QG06MeVg-oDPeaqBsn_IIIpXn81DGFTL52zESP_R7m75NvvL70gEM",
+  collapse_key: "type_a",
+  notification: {
+    body: "Samaan leke aa",
+    title: "https://meet.google.com/sju-ajvo-bdn",
+  },
+});
+
+var config = {
+  method: "post",
+  url: "https://fcm.googleapis.com/fcm/send",
+  headers: {
+    Authorization:
+      "Bearer AAAAIS5xTrk:APA91bGhfh3dr689qPK2PGQZ3BGleALPsbaJcze24ftu4p9M21RU_XTBL6GgWGza5sEl2vEYq8DQgg28gDvX0jUb209xt20HZ_nFUiOViIk3SepGErKJJ0fZ3g-G1uABGfNQ-w0R5do8",
+    "Content-Type": "application/json",
+  },
+  data: data,
+};
--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const shop = require("./../controllers/shop");
-const rider = require('./../controllers/rider')
-const user = require("./../controllers/user")
+const rider = require("./../controllers/rider");
+const user = require("./../controllers/user");
 //reg and login APIs
 app.route("/register_user");
 // app.route("/register_shop");
@@ -13,28 +13,33 @@ app.route("/login_shop");
 app.route("/login_rider");
 
 //shop APIs
-app.route('/shop/list_shop').post(shop.list_shop)
+app.route("/shop/list_shop").post(shop.list_shop);
 app.route("/shop/list_products").post(shop.list_products);
 app.route("/shop/get_products_by_shop_id").get(shop.get_products_by_shop_id);
 // app.route("/shop/stock_update");
-app.route('/shop/set_shop_token').patch(shop.set_fcm_token)
+app.route("/shop/set_shop_token").patch(shop.set_fcm_token);
 
 //user APIs
-app.route("/user/create_user").post(user.create_user)
+app.route("/user/create_user").post(user.create_user);
 app.route("/user/get_all_products").get(shop.get_all_products);
-app.route("/user/get_all_tags").get(shop.get_all_tags)
-app.route("/user/get_product_using_inc_id").get(shop.get_prodcut_using_inc_id)
+app.route("/user/get_all_tags").get(shop.get_all_tags);
+app.route("/user/get_product_using_inc_id").get(shop.get_prodcut_using_inc_id);
 app.route("/add_to_cart");
-app.route("/user/place_order");
 
+// app.route("/user/place_order").get(rider.place_order);
 
 //post_rider
-app.route('/rider/post_rider_details').post(rider.post_details)
-app.route('/rider/set_fcm_token').patch(rider.set_fcm_token)
-app.route('/rider/set_current_coordinates').patch(rider.set_current_coordinates)
+app.route("/rider/post_rider_details").post(rider.post_details);
+app.route("/rider/set_fcm_token").patch(rider.set_fcm_token);
+app
+  .route("/rider/set_current_coordinates")
+  .patch(rider.set_current_coordinates);
 
+app
+  .route("/rider/get_details_using_username")
+  .get(rider.get_details_using_username);
 
-
+app.route("/rider/get_all_riders").get(rider.get_all_riders);
 app.route("/payments");
 
 module.exports = app;
