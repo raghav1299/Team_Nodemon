bed32580d118f7c4cccdec20416f33d8537e2ba9
Atimabh Barunaha <abarunaha@gmail.com>
2021-09-09 03:43:39 +0530

    firebase init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,38 @@
+// import messaging from "./utils/firebaseconfig";
+import { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./shared/Navbar";
 import Home from "./components/Home";
 
+import { getMessaging, onMessage } from "firebase/messaging";
+
 import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 
 function App() {
+  const messaging = getMessaging();
+  onMessage(messaging, (payload) => {
+    console.log("Message received. ", payload);
+    // ...
+  });
+  useEffect(() => {
+    // console.log(messaging);
+  }, []);
+
+  // useEffect(() => {
+  //   if ("serviceWorker" in navigator) {
+  //     navigator.serviceWorker
+  //       .register("../firebase-messaging-sw.js")
+  //       .then(function (registration) {
+  //         console.log("Registration successful, scope is:", registration.scope);
+  //       })
+  //       .catch(function (err) {
+  //         console.log("Service worker registration failed, error:", err);
+  //       });
+  //   }
+  // }, []);
+
   return (
     <div className="App">
       <Router>
---
+++ frontend/src/utils/firebaseconfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getMessaging } from "firebase/messaging/sw";
+
+const config = {
+  apiKey: "api-key",
+  authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://project-id.firebaseio.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "sender-id",
+  appId: "app-id",
+  measurementId: "G-measurement-id",
+};
+
+const app = initializeApp(config);
+const messaging = getMessaging(config);
+
+export default messaging;
