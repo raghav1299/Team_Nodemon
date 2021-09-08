3f7af86f34e59c3dd80d6a87a31b9beb511080ee
Atimabh Barunaha <abarunaha@gmail.com>
2021-09-08 06:29:10 +0530

    add product api

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,24 +1,61 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Dashboard.module.scss";
 
-import { getRequest } from "../utils/api";
+import { getRequest, postRequest } from "../utils/api";
 
 import { Edit2, Trash2 } from "react-feather";
+import ToggleButton from "react-toggle-button";
+import { Plus } from "react-feather";
+import Button from "../shared/Button";
+import Modal from "../shared/Modal";
 
 export default function Dashboard() {
   const [products, setProducts] = useState(null);
+  const [isProductModalOpen, setIsProductModalOpen] = useState(false);
+  const [productDetails, setProductDetails] = useState({
+    shop_id: 1,
+  });
 
-  useEffect(() => {
-    getRequest("/get_all_products")
+  function toggleProductModal() {
+    setIsProductModalOpen((prev) => !prev);
+  }
+
+  function handleAddProductChange(e) {
+    let data = { ...productDetails };
+    data[e.target.name] = e.target.value;
+    setProductDetails(data);
+  }
+
+  function handleProductSubmit(e) {
+    e.preventDefault();
+    console.log(productDetails);
+    postRequest(
+      `/api/shop/list_products?product_name=${productDetails.product_name}&mrp=${productDetails.mrp}&image_address=${productDetails.image_address}&shop_id=1&tags_string=${productDetails.tags_string}&quantity=${productDetails.quantity}`
+    )
       .then((resp) => {
         console.log(resp);
-        setProducts(resp.data.data);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  useEffect(() => {
+    getRequest("/api/shop/get_products_by_shop_id?id=1")
+      .then((resp) => {
+        setProducts(resp.data.data.reverse());
       })
       .catch((err) => console.log(err));
   }, []);
 
   return (
     <div className={styles.wrapper}>
+      <Button
+        className={styles.addProduct}
+        name="Add Product"
+        prefixIcon={<Plus />}
+        width="150px"
+        backgroundColor="#F45C2C"
+        onClick={toggleProductModal}
+      />
       <div className={styles.header}>
         <div className={styles.text}>
           <p>Welcome to your shop! 😃</p>
@@ -26,13 +63,21 @@ export default function Dashboard() {
         </div>
         <div className={styles.controller}>
           <p>Shop Status:</p>
+          <ToggleButton
+            value={true}
+            // onToggle={(value) => {
+            //   self.setState({
+            //     value: !value,
+            //   });
+            // }}
+          />
         </div>
       </div>
       <div className={styles.products}>
         {products &&
           products.length &&
-          products.map((prod) => (
-            <div className={styles.product}>
+          products.map((prod, index) => (
+            <div className={styles.product} key={index}>
               <div className={styles.image}>
                 <img src={prod.image_address} alt="" />
               </div>
@@ -61,6 +106,29 @@ export default function Dashboard() {
             </div>
           ))}
       </div>
+
+      <Modal
+        isOpen={isProductModalOpen}
+        title="Add Product"
+        onClose={toggleProductModal}
+        allowClose
+      >
+        <div className={styles.productForm}>
+          <form onChange={handleAddProductChange} onSubmit={handleProductSubmit}>
+            <label>Product Name</label>
+            <input type="text" name="product_name" />
+            <label>Product MRP</label>
+            <input type="number" name="mrp" />
+            <label>Quantity</label>
+            <input type="text" name="quantity" />
+            <label>Image URL</label>
+            <input type="text" name="image_address" />
+            <label>Tags</label>
+            <input type="text" name="tags_string" />
+            <Button name="Submit" backgroundColor="#F45C2C" />
+          </form>
+        </div>
+      </Modal>
     </div>
   );
 }
--- frontend/src/shared/Button.js
+++ frontend/src/shared/Button.js
@@ -1,11 +1,17 @@
 import React from "react";
 import styles from "./Button.module.scss";
+import clsx from "clsx";
 
 export default function Button(props) {
   return (
     <button
-      className={styles.button}
-      style={{ width: props.width, height: props.height, backgroundColor: props.backgroundColor, color: props.color }}
+      className={clsx(styles.button, props.className)}
+      style={{
+        width: props.width,
+        height: props.height,
+        backgroundColor: props.backgroundColor,
+        color: props.color,
+      }}
       onClick={props.onClick}
       type={props.type}
     >
