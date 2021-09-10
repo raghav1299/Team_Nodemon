43eb4cc9089665721d3efb071134b5e0e45e7bc2
Raghav Sharma <raghavsharma1299@gmail.com>
2021-09-10 18:10:51 +0530

    loader, refresh and bugs fixes

--- App/Nodemon_RiderApp/src/screens/Home.js
+++ App/Nodemon_RiderApp/src/screens/Home.js
@@ -9,6 +9,7 @@ import {
     heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Loader } from "../Components/Components";
 import { storeData } from '../store/asyncStore';
 
 export default Home = ({ navigation }) => {
@@ -16,34 +17,47 @@ export default Home = ({ navigation }) => {
 
 
 
-    const [username, setUsername] = useState('')
+    const [username, setUsername] = useState('');
+    const [isLoading, setLoading] = useState(false);
 
 
 
     function getDetail() {
-        axios.get(`${BASE_URL_WEB}/api/rider/get_details_using_username`, {
-            params: {
-                "username": username
-            }
-        }).then((res) => {
-            console.log("get user data username", res.data.data)
-            let value = res.data.data
-            storeData("userDetail", value)
-            //console.log(store.fcmToken)
-            axios.patch(`${BASE_URL_WEB}/api/rider/set_fcm_token?token=${store.fcmToken}&inc_id=${res.data.data.inc_id}`
-            ).then((res) => {
-                navigation.navigate('orderScreen')
-                //console.log(res.data)
+        if (username != '') {
+            setLoading(true)
+            axios.get(`${BASE_URL_WEB}/api/rider/get_details_using_username`, {
+                params: {
+                    "username": username
+                }
+            }).then((res) => {
+                console.log("get user data username", res.data.data)
+                let value = res.data.data
+                storeData("userDetail", value)
+                //console.log(store.fcmToken)
+                axios.patch(`${BASE_URL_WEB}/api/rider/set_fcm_token?token=${store.fcmToken}&inc_id=${res.data.data.inc_id}`
+                ).then((res) => {
+
+                    navigation.navigate('orderScreen')
+                    setUsername('')
+                    setLoading(false)
+                    //console.log(res.data)
+                }).catch((err) => {
+                    console.log(err)
+                })
             }).catch((err) => {
+                alert(err)
                 console.log(err)
             })
-        }).catch((err) => {
-            console.log(err)
-        })
+        }
+        else {
+            alert("please enter username")
+        }
     }
 
 
-    return (
+    return isLoading ? (
+        <Loader />
+    ) : (
         <View style={{ flex: 1, justifyContent: 'center', backgroundColor: 'white' }}>
             <View style={{ flex: 1.5 }}>
                 <LottieView source={require('../assets/lottie.json')} autoPlay loop />
@@ -63,6 +77,7 @@ export default Home = ({ navigation }) => {
             <TextInput
                 style={styles.input}
                 placeholder={'Username'}
+                value={username}
                 onChangeText={(input) => { setUsername(input) }} />
             <TouchableOpacity
                 style={styles.button}
@@ -70,6 +85,12 @@ export default Home = ({ navigation }) => {
                     getDetail()
                 }}>
                 <Text style={styles.TouchableText}>Login</Text></TouchableOpacity>
+            {/* <TouchableOpacity
+                style={styles.buttonregister}
+                onPress={() => {
+                    navigation.navigate('register')
+                }}>
+                <Text style={styles.TouchableTextRegister}>Register</Text></TouchableOpacity> */}
         </View>
     )
 }
@@ -82,6 +103,14 @@ const styles = StyleSheet.create({
         marginVertical: "6%",
         backgroundColor: COLORS.CART_ORANGE
     },
+    buttonregister: {
+        width: "80%",
+        height: 30,
+        borderRadius: 10,
+        alignSelf: "center",
+        marginBottom: 30,
+        marginTop: -15,
+    },
     TouchableText: {
         alignSelf: "center",
         textAlignVertical: "center",
@@ -90,7 +119,16 @@ const styles = StyleSheet.create({
         fontSize: 16,
         color: "#ffffff"
     },
+    TouchableTextRegister: {
+        alignSelf: "center",
+        textAlignVertical: "center",
+        flex: 1,
+        fontFamily: "Inter-Medium",
+        fontSize: 16,
+        color: "black"
+    },
     input: {
+        paddingLeft: 15,
         alignSelf: 'center',
         width: "80%",
         height: 40,
